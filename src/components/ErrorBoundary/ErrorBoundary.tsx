3811cacff032c6d5f535473369ea30cc38e797f8
l1senochek <lisenochek96@gmail.com>
2025-01-29 15:11:39 +0300

    feat: install sass & alias / add ErrorBoundary & Fallback

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,39 @@
+import { Component, ReactNode } from 'react';
+import Fallback from '@/components/Fallback/Fallback.tsx';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+  error: Error | null;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: null,
+    };
+  }
+
+  public static getDerivedStateFromError(error: Error) {
+    return { hasError: true, error };
+  }
+
+  private resetError = () => {
+    this.setState({ hasError: false, error: null });
+  };
+
+  public render() {
+    if (this.state.hasError) {
+      return <Fallback error={this.state.error} onReset={this.resetError} />;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary.tsx';
 
 const root = document.getElementById('root');
 
 if (root)
   createRoot(root).render(
     <StrictMode>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </StrictMode>
   );
